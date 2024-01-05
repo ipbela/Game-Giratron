91ee3a6f7488c32e57b9d8e887bfe0a3e1320cad
Isabela Pereira <75582394+ipbela@users.noreply.github.com>
2024-01-05 16:45:31 -0300

    adding comments

--- Gamer.py
+++ Gamer.py
@@ -1,10 +1,9 @@
 class Gamer:
     def __init__(self):
+        # the player chooses a name
         while True:
             self.name_gamer = input("Enter with the name: \n").upper()
             if all(c.isalpha() or c.isspace() for c in self.name_gamer):
                 break
             else:
                 print("\nPlease enter with only letters\n")
-
-        self.game_lives = 5
